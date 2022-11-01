f986a270cf83ebffb8c1d982875dd3d65c977073
soichiroooo <slamdunkducksky@gmail.com>
2022-11-01 12:37:29 +0900

    キー名を修正

--- db/migrate/20221031033706_create_questions.rb
+++ db/migrate/20221031033706_create_questions.rb
@@ -3,7 +3,7 @@ class CreateQuestions < ActiveRecord::Migration[6.0]
     create_table :questions do |t|
       t.string :title
       t.text :content
-      t.string :name
+      t.string :questioner
       t.timestamps
     end
   end
