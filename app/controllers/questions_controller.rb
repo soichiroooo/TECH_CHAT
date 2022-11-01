8c9d35e250cb18c8de096d0ce6ba516af5534518
soichiroooo <slamdunkducksky@gmail.com>
2022-11-01 11:46:25 +0900

    コントローラーにcreateアクションを定義

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+  
   def index
     @questions = Question.all
   end
@@ -6,5 +7,14 @@ class QuestionsController < ApplicationController
   def new
     @question = Question.new
   end
-  
+
+  def create
+    Question.create(question_params)
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :content, :questioner)
+  end
+
 end
