e9bea980e775b899503a070a4e46f5c9da3c7b2b
soichiroooo <slamdunkducksky@gmail.com>
2022-11-01 11:32:17 +0900

    誤って削除してしまったdbファイル内のカラム追加の記述を再度記載

--- db/migrate/20221031033706_create_questions.rb
+++ db/migrate/20221031033706_create_questions.rb
@@ -1,7 +1,9 @@
 class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
-
+      t.string :title
+      t.text :content
+      t.string :name
       t.timestamps
     end
   end
