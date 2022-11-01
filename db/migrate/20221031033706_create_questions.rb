d1a98383fcf0832d1f9dd282b0e3c3cf944d1349
soichiroooo <slamdunkducksky@gmail.com>
2022-11-01 10:40:54 +0900

    dbにカラム追加

--- db/migrate/20221031033706_create_questions.rb
+++ db/migrate/20221031033706_create_questions.rb
@@ -1,7 +1,9 @@
 class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
-
+      t.string :title
+      t.text :content
+      t.string :questioner
       t.timestamps
     end
   end
